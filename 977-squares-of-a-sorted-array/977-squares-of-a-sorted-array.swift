5ddd9fda9dfb752cdefc31b2c26f7d023e6da701
Ruyther Costa <ruy_fusion@hotmail.com>
2022-08-16 21:59:01 -0300

    Time: 459 ms (7.19%), Space: 15.8 MB (29.47%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.swift
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.swift
@@ -1,5 +1,19 @@
 class Solution {
     func sortedSquares(_ nums: [Int]) -> [Int] {
-        return nums.map { $0 * $0 }.sorted()
+        var left = 0
+        var right = nums.count - 1
+        var result = [Int](repeating: 0, count: nums.count)
+        
+        for i in 0..<nums.count {
+            var curLeft = nums[left] * nums[left]
+            var curRight = nums[right] * nums[right]
+            
+            result[nums.count - (i + 1)] = max(curLeft, curRight)
+            
+            left += curLeft >= curRight ? 1 : 0
+            right -= curLeft < curRight ? 1 : 0
+        }
+
+        return result
     }
 }
