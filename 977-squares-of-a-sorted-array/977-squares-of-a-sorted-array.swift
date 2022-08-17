68b4dccfe6ecd8d59446eb116c89f554081c8544
Ruyther Costa <ruy_fusion@hotmail.com>
2022-08-16 21:40:03 -0300

    Time: 350 ms (44.04%), Space: 15.8 MB (13.33%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.swift
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.swift
@@ -1,19 +1,5 @@
 class Solution {
     func sortedSquares(_ nums: [Int]) -> [Int] {
-        var result = Array(repeating: 0, count: nums.count)
-        var i = 0
-        var j = nums.count - 1
-        var k = j
-        while i <= j {
-            if abs(nums[i]) > abs(nums[j]) {
-                result[k] = nums[i] * nums[i]
-                i += 1
-            } else {
-                result[k] = nums[j] * nums[j]
-                j -= 1
-            }
-            k -= 1
-        }
-        return result
+        return nums.map { $0 * $0 }.sorted()
     }
 }
