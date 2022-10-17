ebf56f91b54e55ef79ffa45c6b5284cbdd170b70
Ruyther Costa <ruy_fusion@hotmail.com>
2022-10-16 22:13:29 -0300

    Time: 303 ms (79.26%), Space: 14.6 MB (13.73%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+        
+        while left <= right {
+            let pivot = right - left/2
+            
+            if nums[pivot] == target {
+                return pivot
+            } else if nums[pivot] > target  {
+                right = pivot - 1
+            } else {
+                left = pivot + 1
+            }
+        }
+        
+        return -1
+    }
+}
