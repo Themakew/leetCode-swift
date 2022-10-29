0ec59ed89b74f3bf54cd94685a06327c8832d388
Ruyther Costa <ruy_fusion@hotmail.com>
2022-10-29 16:34:49 -0300

    Time: 6 ms (48.50%), Space: 14.4 MB (47.80%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.swift
+++ 0020-valid-parentheses/0020-valid-parentheses.swift
@@ -1,3 +1,23 @@
+final class ListNode {
+    var val: Int
+    var next: ListNode?
+
+    init() {
+        self.val = 0
+        self.next = nil
+    }
+
+    init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+
+    public init(_ val: Int, _ next: ListNode?) {
+        self.val = val
+        self.next = next
+    }
+}
+
 class Solution {
     func isValid(_ s: String) -> Bool {
         var stack = [Character]()
