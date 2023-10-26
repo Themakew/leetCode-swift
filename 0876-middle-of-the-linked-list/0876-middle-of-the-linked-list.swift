58ab132a319795f05b4ba9516b29ae26f7bf8e78
Ruyther Costa <ruy_fusion@hotmail.com>
2023-10-26 16:05:25 -0600

    Time: 4 ms (52.62%), Space: 13.9 MB (73.55%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.swift
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        guard head != nil else {
+            return nil
+        }
+        
+        guard head?.next != nil else {
+            return head
+        }
+        
+        var fastPointer: ListNode? = head
+        var slowPointer: ListNode? = head
+        
+        while fastPointer != nil && fastPointer?.next != nil {
+            fastPointer = fastPointer?.next?.next
+            slowPointer = slowPointer?.next
+        }
+        
+        return slowPointer
+    }
+}
