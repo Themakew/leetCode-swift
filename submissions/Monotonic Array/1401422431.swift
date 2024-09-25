76b79d5066f67b5dd904bb241f41d0e9af9a8333
Ruyther Costa <ruy_fusion@hotmail.com>
2024-09-24 21:33:21 -0600

    Add submission 1401422431

---
+++ submissions/Monotonic Array/1401422431.swift
@@ -0,0 +1,35 @@
+# Title: Monotonic Array
+# Submission ID: 1401422431
+# Status: Wrong Answer
+# Date: September 24, 2024 at 09:33:14 PM MDT
+
+class Solution {
+    func isMonotonic(_ nums: [Int]) -> Bool {
+        guard nums.count > 1 else {
+            return true
+        }
+
+        var isIncreasing = false
+        var isDecreasing = false
+
+        for index in 1..<nums.count {
+            if nums[index] == nums[index - 1] {
+                continue
+            }
+
+            if !isIncreasing && !isDecreasing {
+                if nums[index] > nums[index - 1] {
+                    isIncreasing = true
+                } else {
+                    isDecreasing = true
+                }
+            } else if isIncreasing && nums[index] > nums[index - 1] {
+                return false
+            } else if isDecreasing && nums[index] < nums[index - 1] {
+                return false
+            }
+        }
+
+        return true
+    }
+}
